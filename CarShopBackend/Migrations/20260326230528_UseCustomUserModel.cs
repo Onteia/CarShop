e26ca562c5561a5e9aba0d92b7cdc86e3b66d871
Onteia <onteialexa@gmail.com>
2026-03-26 16:19:00 -0700

    Create custom identity containing references to Wishlist and Cart

---
+++ CarShopBackend/Migrations/20260326230528_UseCustomUserModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarShopBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UseCustomUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CartID",
+                table: "AspNetUsers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WishlistID",
+                table: "AspNetUsers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CartID",
+                table: "AspNetUsers",
+                column: "CartID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_WishlistID",
+                table: "AspNetUsers",
+                column: "WishlistID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Carts_CartID",
+                table: "AspNetUsers",
+                column: "CartID",
+                principalTable: "Carts",
+                principalColumn: "CartID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Wishlists_WishlistID",
+                table: "AspNetUsers",
+                column: "WishlistID",
+                principalTable: "Wishlists",
+                principalColumn: "WishlistID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Carts_CartID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Wishlists_WishlistID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CartID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_WishlistID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CartID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WishlistID",
+                table: "AspNetUsers");
+        }
+    }
+}
