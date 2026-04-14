9a7f02da8f693d9649f40a8ae92039671f764511
Onteia <onteialexa@gmail.com>
2026-04-13 19:13:00 -0700

    Design listing page

--- car-shop-frontend/src/app/listing/[listingID]/page.tsx
+++ car-shop-frontend/src/app/listing/[listingID]/page.tsx
@@ -1,3 +1,116 @@
-export default function Page() {
-    return <h1>listing page</h1>;
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { ListingModel } from "@/types";
+import { useRouter } from "next/navigation";
+import { use } from "react";
+
+async function fetchListing(id: string): Promise<ListingModel> {
+    const response = await fetch(`http://localhost:5011/listings/${id}`, { mode: "cors" });
+    return response.json();
+}
+
+function getListingPrice(listing: ListingModel | undefined) {
+    if (listing === undefined) return 0;
+    if (listing.saleAmount === undefined || listing.saleAmount === 0) return listing.listPrice;
+
+    return (100 - (listing.saleAmount)) / 100 * listing.listPrice;
+}
+
+export default function Page({ params }: { params: Promise<{ listingID: string }> }) {
+    const { listingID } = use(params);
+    const { data, error, isLoading, isError } = useQuery({
+        queryKey: ["listing"],
+        queryFn: () => fetchListing(listingID)
+    });
+
+    if (isLoading) return <p>Loading...</p>;
+    if (isError) return <p>{(error as Error).message}</p>;
+
+    return (
+        <div className="bg-white">
+            <div className="pt-6">
+                <nav aria-label="Breadcrumb">
+                    <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                        <li>
+                            <div className="flex items-center">
+                                <a href="#" className="mr-2 text-sm font-medium text-gray-900">{data?.vehicle.vehicleType.typeName}</a>
+                                <svg viewBox="0 0 16 20" width="16" height="20" fill="currentColor" aria-hidden="true" className="h-5 w-4 text-gray-300">
+                                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                                </svg>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="flex items-center">
+                                <a href="#" className="mr-2 text-sm font-medium text-gray-900">{data?.vehicle.vehicleMake.makeName}</a>
+                                <svg viewBox="0 0 16 20" width="16" height="20" fill="currentColor" aria-hidden="true" className="h-5 w-4 text-gray-300">
+                                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                                </svg>
+                            </div>
+                        </li>
+
+                        <li className="text-sm">
+                            <a href="#" aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">{data?.vehicle.model}</a>
+                        </li>
+                    </ol>
+                </nav>
+
+                <div className="mx-auto max-w-2xl px-4 pt-10 pb-6 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto_auto_1fr] lg:gap-x-8 lg:px-8 lg:pt-16">
+                    <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+                        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{data?.name} {data?.vehicle.isUsed && <u>(USED)</u>}</h1>
+                    </div>
+
+                    <div className="mt-4 lg:row-span-3 lg:mt-0">
+                        <h2 className="sr-only">Vehicle information</h2>
+                        <p className="text-3xl tracking-tight text-gray-900">{(getListingPrice(data)).toLocaleString("en-US", { style: "currency", currency: "USD" })}
+                            {(data?.listPrice !== getListingPrice(data)) && <span style={{ whiteSpace: "pre", fontSize: 12, verticalAlign: "center" }}>    List: <s>{data?.listPrice}</s></span>}
+                        </p>
+
+                        <div className="mt-6">
+                            <h3 className="sr-only">Reviews</h3>
+                            <div className="flex items-center">
+                                <div className="flex items-center">
+                                    <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5 shrink-0 text-gray-900">
+                                        <path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" fillRule="evenodd" />
+                                    </svg>
+                                    <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5 shrink-0 text-gray-900">
+                                        <path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" fillRule="evenodd" />
+                                    </svg>
+                                    <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5 shrink-0 text-gray-900">
+                                        <path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" fillRule="evenodd" />
+                                    </svg>
+                                    <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5 shrink-0 text-gray-900">
+                                        <path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" fillRule="evenodd" />
+                                    </svg>
+                                    <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5 shrink-0 text-gray-200">
+                                        <path d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clipRule="evenodd" fillRule="evenodd" />
+                                    </svg>
+                                </div>
+                                <p className="sr-only">4 out of 5 stars</p>
+                                <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reviews</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pr-8 ">
+                        <div>
+                            <h3 className="sr-only">Description</h3>
+
+                            <div className="space-y-6">
+                                <p className="text-base text-gray-900">{`The ${data?.vehicle.model} (${data?.vehicle.year}) is a(n) ${data?.vehicle.vehicleType.typeName} made by the trusted ${data?.vehicle.vehicleMake.makeName}`}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-8 lg:px-8">
+                    {data?.vehicle.images.map((image, key) => (
+                        <img key={key} src={image.imageURI} className="row-span-2 aspect-3/4 size-full rounded-lg object-cover max-lg:hidden" />
+                    ))}
+                </div>
+
+            </div>
+        </div>
+
+    );
 }
