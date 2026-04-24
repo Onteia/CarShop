a8073934e1ff427398ad3fd1731b3f7704fa0f96
Onteia <onteialexa@gmail.com>
2026-04-23 17:08:56 -0700

    Implement frontend for home page and transaction success and error pages

--- car-shop-frontend/src/app/page.tsx
+++ car-shop-frontend/src/app/page.tsx
@@ -1,65 +1,68 @@
-import Image from "next/image";
+import { BanknotesIcon, ChatBubbleLeftRightIcon, ShoppingBagIcon, TruckIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link';
+
+const features = [
+  {
+    name: 'Speedy delivery times',
+    description:
+      'We guarantee same-day vehicle deliveries. Yes, you heard that right.',
+    icon: TruckIcon,
+  },
+  {
+    name: 'Top-of-the-line shopping experience',
+    description:
+      'Purchase and save your favorite vehicles to your wishlist without a headache.',
+    icon: ShoppingBagIcon,
+  },
+  {
+    name: 'World-class customer support',
+    description:
+      'Talk to our best representatives to solve any issues you encounter.',
+    icon: ChatBubbleLeftRightIcon,
+  },
+  {
+    name: 'Thriving trade-in ecosystem',
+    description:
+      'Exchange your used vehicle for something your heart desires for cheap.',
+    icon: BanknotesIcon,
+  },
+]
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="grow bg-zinc-50 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base/7 font-semibold text-indigo-600">Drive faster</h2>
+          <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl lg:text-balance">
+            Get your dream vehicle with ease
+          </p>
+          <p className="mt-6 text-lg/8 text-gray-700">
+            Why wait? You can order your dream car and put it on the road in the same day!
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base/7 font-semibold text-gray-900">
+                  <div className="absolute top-0 left-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                    <feature.icon aria-hidden="true" className="size-6 text-white" />
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base/7 text-gray-600">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
         </div>
-      </main>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max0w04xl grid grid-flow-col justify-end">
+          <Link href="/shop" className="rounded-md text-zinc-50 bg-indigo-600 border border-transparent shadow-xs px-6 py-3 font-medium hover:bg-indigo-700 hover:cursor-pointer">
+            Shop now!
+          </Link>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
--- car-shop-frontend/src/app/shop/page.tsx
+++ car-shop-frontend/src/app/shop/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   const listings = await getListings();
 
   return (
-    <div className="bg-white">
+    <div className="grow bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Listings</h2>
 
