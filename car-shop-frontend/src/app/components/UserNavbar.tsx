4cd7a468714d14134d362008c74b47182a9342de
Onteia <onteialexa@gmail.com>
2026-04-30 12:43:47 -0700

    Make user navbar react on hover rather than click

--- car-shop-frontend/src/app/components/UserNavbar.tsx
+++ car-shop-frontend/src/app/components/UserNavbar.tsx
@@ -1,22 +1,28 @@
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
 import Link from "next/link";
 import { redirect, RedirectType } from "next/navigation";
-import { use } from "react";
+import { use, useState } from "react";
 import { Button } from "react-aria-components";
 import { LogOutUser } from "../actions/userActions";
 
 export default function UserNavbar() {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+    const openMenu = () => {
+        setMenuOpen(true);
+    };
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
     return (
-        <Popover>
-            <PopoverButton style={{ cursor: "pointer" }} className="block text-sm/6 font-medium text-gray-900 focus:outline-none data-active:text-white data-focus:outline data-focus:outline-white data-hover:text-white">
-                <div className="-m-2 block p-2 font-medium text-gray-700 hover:text-gray-800">My Account</div>
-            </PopoverButton>
-            <PopoverPanel
-                transition
-                anchor="bottom"
-                className="divide-y divide-white/5 rounded-xl bg-white text-sm/6 transition duration-200 ease-in-out [--anchor-gap:--spacing(5)] data-closed:-translate-y-1 data-closed:opacity-0"
+        <div onMouseEnter={openMenu} onMouseLeave={closeMenu}>
+            <div style={{ cursor: "pointer" }} className="block text-sm/6 font-medium text-gray-900 focus:outline-none data-active:text-white data-focus:outline data-focus:outline-white data-hover:text-white">
+                <div className="-m-2 block p-2 font-medium text-center text-gray-700 hover:text-gray-800">My Account</div>
+            </div>
+            <div
+                className="fixed divide-y divide-white/5 border rounded-xl bg-white text-sm/6 transition duration-200 ease-in-out [--anchor-gap:--spacing(5)] data-closed:-translate-y-1 data-closed:opacity-0"
             >
-                <div className="p-3">
+                <div hidden={!menuOpen} className="p-1">
                     <Link className="block rounded-lg px-3 py-2 transition hover:bg-black/10 text-gray-700 hover:text-gray-800" href="/user/cart">
                         <p>Cart</p>
                     </Link>
@@ -27,7 +33,7 @@ export default function UserNavbar() {
                         <p>Log Out</p>
                     </Button>
                 </div>
-            </PopoverPanel>
-        </Popover>
+            </div>
+        </div>
     );
 }
